b57f3a79a6d5a26c158ce57fcd45e4115efc68e9
dintharu <Laknathoki@gmail.com>
2026-01-28 00:52:03 -0800

    fix: update the AppNavigator.tsx which will easil directed the user to chatScreen.tsx

--- AwesomeProject/src/AppNavigator.tsx
+++ AwesomeProject/src/AppNavigator.tsx
@@ -12,7 +12,7 @@ import LoginGuardianScreen from './screens/LoginGuardianScreen';
 import ForgotPasswordScreen from './screens/ForgotPasswordScreen';
 import VerifyCodeScreen from './screens/VerifyCodeScreen';
 import HomeScreen from './screens/HomeScreen';
-import ChatScreen from './screens/ChatScreen'; // NEW IMPORT
+import ChatScreen from './screens/ChatScreen';
 import FocusModeScreen from './screens/FocusModeScreen';
 import TasksScreen from './screens/TasksScreen';
 import CaregiverDashboard from './screens/CaregiverDashboard';
@@ -28,31 +28,20 @@ const AppNavigator = () => {
           headerShown: false,
         }}
       >
-        {/* Onboarding Screens */}
         <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="Features" component={FeaturesScreen} />
         <Stack.Screen name="Privacy" component={PrivacyScreen} />
-        {/* Authentication Screens */}
         <Stack.Screen name="RoleSelection" component={RoleSelectionScreen} />
         <Stack.Screen name="CreateAccount" component={CreateAccountScreen} />
         <Stack.Screen name="LoginUser" component={LoginUserScreen} />
         <Stack.Screen name="LoginGuardian" component={LoginGuardianScreen} />
         <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
         <Stack.Screen name="VerifyCode" component={VerifyCodeScreen} />
-        {/* Main App Screens */}
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen
-          name="Chat"
-          component={ChatScreen}
-          options={{
-            animation: 'slide_from_right', // Smooth transition
-          }}
-        />{' '}
-        {/* NEW SCREEN */}
+        <Stack.Screen name="Chat" component={ChatScreen} />
         <Stack.Screen name="FocusMode" component={FocusModeScreen} />
         <Stack.Screen name="Tasks" component={TasksScreen} />
-        {/* Caregiver Screens */}
         <Stack.Screen
           name="CaregiverDashboard"
           component={CaregiverDashboard}
