850b89a85cc17f7bbc4541621a78926d18001a8b
dintharu <Laknathoki@gmail.com>
2026-03-18 04:22:35 +0530

    fixed some bugs

--- Frontend/metro.config.js
+++ Frontend/metro.config.js
@@ -1,2 +1,11 @@
-const { getDefaultConfig } = require('expo/metro-config');
-module.exports = getDefaultConfig(__dirname);
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
